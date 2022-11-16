f95664c7086e32ec30ecc8f259c8961879841de5
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-11-16 15:29:35 +0100

    googleMapsUrl, title, description

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,15 +5,21 @@ const Card = (props) => {
     console.log(props)
   return (
     <div className='card'>
-        <div className='card--image'>
+        <section className='card--image'>
             <img src={props.item.imageUrl} alt="image" className='card--image'/>
             <div>
                 <div>
                     <img src={location}/>
                     <span className='card--country'>{props.item.location}</span>
+                    <a href={props.item.googleMapsUrl} className="card--geolink">View on Google Maps</a>
                 </div>
+                <div>
+                    <h1>{props.item.title}</h1>
+                </div>
+                    <h3>{props.item.startDate} - {props.item.endDate}</h3>
+                    <p>{props.item.description}</p>
             </div>
-        </div>
+        </section>
 
     </div>
   )
