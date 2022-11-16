ec96fb3e4e375078d4027f2eadd9e4d015e3c855
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-11-16 11:00:38 +0100

    egyptian reefs

--- src/data.js
+++ src/data.js
@@ -27,6 +27,15 @@ export default [
         description: "Mount Vesuvius forms an iconic backdrop to the Bay of Naples, Italy, and is one of Europe's most active volcanoes. It is best known for an eruption in AD 79 that buried the Roman settlements of Pompeii and Herculaneum under metres of ash.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
     },
+    {
+        title: "Coral Reefs",
+        location: "Egypt",
+        googleMapsUrl: "https://goo.gl/maps/nfqF7jYGxPhwKi6E7",
+        startDate: "22 Jul, 2010",
+        endDate: "30 Jul, 2010",
+        description: "Shaab El Erg is a horseshoe-shaped reef (about 7 KM in length) with at the center a small lagoon scattered with ergs. It is also one of the natural habitats in the Red Sea of a large group of Spinner dolphins.",
+        imageUrl: "https://unsplash.com/photos/2mH5fEFYc5s",
+    },
 
 
 ]
