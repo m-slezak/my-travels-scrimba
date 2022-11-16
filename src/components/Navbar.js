070070ecab976d12ce29abbf8e86eec57e63b609
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-11-16 10:27:14 +0100

    Navbar section ready

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from '../assets/planet.png'
 
 const Navbar = () => {
   return (
-    <div><h1>Hello, WOrld!</h1></div>
+    <div className='navbar'>
+        <img src={logo} alt="World" height="24px" width="24px" />
+        <h3 className='navbar--heading'>my travel journal.</h3>
+
+    </div>
   )
 }
 
