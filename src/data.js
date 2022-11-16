092dc767df4ead763eb0c218377c552c793aa0a3
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-11-16 11:25:30 +0100

    Park Guell, Positiano add

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Positiano",
+        location: "Italy",
+        googleMapsUrl: "https://goo.gl/maps/tbPAb9Qoxmp4LeLcA",
+        startDate: "12 Mar, 2018",
+        endDate: "24 Mar, 2018",
+        description: "Positano is suspended between sky and sea, also known as vertical city as it grows along the slopes of the mountain that dominates.",
+        imageUrl: "https://unsplash.com/photos/z0L0mo__9bg"
     },
 
         {
@@ -36,6 +36,14 @@ export default [
         description: "Shaab El Erg is a horseshoe-shaped reef (about 7 KM in length) with at the center a small lagoon scattered with ergs. It is also one of the natural habitats in the Red Sea of a large group of Spinner dolphins.",
         imageUrl: "https://unsplash.com/photos/2mH5fEFYc5s",
     },
-
+    {
+        title: "Park Guell",
+        location: "Spain",
+        googleMapsUrl: "https://g.page/ParkGuellOficial?share",
+        startDate: "13 Aug, 2006",
+        endDate: "23 Aug, 2006",
+        description: "Park Guell is one of the most impressive public parks in the world. The park is located in Barcelona and was designed by famous architect Antonio Gaudi.",
+        imageUrl: "https://unsplash.com/photos/qiH16d5SRxg",
+    },
 
 ]
