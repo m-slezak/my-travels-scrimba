1eb4feffc705f0c1de380951e99974be0bd44d9e
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-11-16 10:45:24 +0100

    Vesuvius info add

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+
+        {
+        title: "Vesuvius",
+        location: "Italy",
+        googleMapsUrl: "https://goo.gl/maps/5b1zSLFiGRPRkWrDA",
+        startDate: "08 Jul, 2018",
+        endDate: "18 Jul, 2018",
+        description: "Mount Vesuvius forms an iconic backdrop to the Bay of Naples, Italy, and is one of Europe's most active volcanoes. It is best known for an eruption in AD 79 that buried the Roman settlements of Pompeii and Herculaneum under metres of ash.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    },
+    {
+        title: "Vesuvius",
+        location: "Italy",
+        googleMapsUrl: "https://goo.gl/maps/5b1zSLFiGRPRkWrDA",
+        startDate: "08 Jul, 2018",
+        endDate: "18 Jul, 2018",
+        description: "Mount Vesuvius forms an iconic backdrop to the Bay of Naples, Italy, and is one of Europe's most active volcanoes. It is best known for an eruption in AD 79 that buried the Roman settlements of Pompeii and Herculaneum under metres of ash.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    },
+
+
+]
